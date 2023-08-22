6652e967e2eefa4510b7808dad8e989757cfc782
Rajkumar O <raj4learn@gmail.com>
2023-08-22 18:14:25 +0530

    Change in Comparation

--- src/pages/ImageItems.js
+++ src/pages/ImageItems.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 import './ImageItems.css' 
 class ImageItems extends Component {
     render() {
-        const isActive = this.props.index === '1' ? "carousel-item active" : "carousel-item"
+        const isActive = this.props.index === 1 ? "carousel-item active" : "carousel-item"
         return (
             <div className={isActive}>
                 <div className='row'>
                     <div className='col-sm-12'>
                         <div className='banner_taital'>
-                            <img className='image setFreezImgSize' alt="Avatar" src={this.props.imageName}></img>
+                            <img className='image setFreezImgSize' alt="Avatar" src={this.props.imageName} />
                         </div>
                     </div>
                 </div>
