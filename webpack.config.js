4577ca7ec8e21061dfe43e461f792b7a41fb15d3
Rajkumar O <raj4learn@gmail.com>
2023-08-30 07:59:39 +0530

    Webpack commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,58 @@
 const path = require('path')
+const webpack = require('webpack');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const port = process.env.PORT || 3000;
+
 module.exports = {
-    module: 'speedxerox',
+    // Webpack configuration goes here
+    module: {
+        name: 'speedxerox',
+        rules: [
+
+            // First Rule
+            {
+                test: /\.(js)$/,
+                exclude: /node_modules/,
+                use: ['babel-loader']
+            },
+
+            // Second Rule
+            {
+                test: /\.css$/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true,
+                            localsConvention: 'camelCase',
+                            sourceMap: true
+                        }
+                    }
+                ]
+            }
+        ]
+    },
     path: path.reslove(__dirname, 'build'),
-    publicPath: '.'
-}
+    publicPath: '.',
+    mode: 'development',
+    entry: './src/index.js',
+    output: {
+        filename: 'bundle.[hash].js'
+    },
+    devtool: 'inline-source-map',
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            favicon: 'public/favicon.ico'
+        })
+    ],
+    devServer: {
+        host: 'localhost',
+        port: port,
+        historyApiFallback: true,
+        open: true
+    }
+};
